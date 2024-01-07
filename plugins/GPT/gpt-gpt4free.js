4fef013151223cae23d4bd6b88ff510e0ec2bc5d
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-01-07 17:49:18 +0800

    Add files via upload

--- plugins/GPT/gpt-gpt4free.js
+++ plugins/GPT/gpt-gpt4free.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+import cheerio from "cheerio";
 
 let handler = async (m, {
     conn,
@@ -28,23 +29,24 @@ export default handler
 /* New Line */
 async function generate(q) {
     try {
-        const {
-            data
-        } = await axios(
-            `https://gpt4free.io/wp-json/mwai-ui/v1/chats/submit`, {
-                method: "post",
-                data: {
-                    botId: "default",
-                    newMessage: q,
-                    stream: false,
-                },
-                headers: {
-                    "X-WP-Nonce": "d225477020",
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        return data;
+        const response = await axios.get("https://gpt4free.io/chat");
+        const htmlData = response.data;
+
+        const $ = cheerio.load(htmlData);
+        const restNonce = JSON.parse($('.mwai-chatbot-container').attr('data-system')).restNonce;
+
+        const submitResponse = await axios.post("https://gpt4free.io/wp-json/mwai-ui/v1/chats/submit", {
+            botId: "default",
+            newMessage: "hy",
+            stream: false,
+        }, {
+            headers: {
+                "X-WP-Nonce": restNonce,
+                "Content-Type": "application/json",
+            },
+        });
+
+        return submitResponse.data;
     } catch (err) {
         console.log(err.response.data);
         return err.response.data.message;

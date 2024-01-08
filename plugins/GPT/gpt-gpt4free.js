a4f3214ec44a608cbbe4005b618dd62f3998e6c5
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-01-09 00:02:32 +0800

    Add files via upload

--- plugins/GPT/gpt-gpt4free.js
+++ plugins/GPT/gpt-gpt4free.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import cheerio from "cheerio";
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 let handler = async (m, {
     conn,
@@ -16,7 +16,7 @@ let handler = async (m, {
     await m.reply(wait)
     try {
         let result = await generate(text)
-        await m.reply(result.reply)
+        await m.reply(result)
     } catch (e) {
         await m.reply(eror)
     }
@@ -29,24 +29,27 @@ export default handler
 /* New Line */
 async function generate(q) {
     try {
-        const response = await axios.get("https://gpt4free.io/chat");
-        const htmlData = response.data;
+        const nonceValue = JSON.parse(cheerio.load(await (await axios.get(
+            "https://gpt4free.io/chat"
+        )).data)('.mwai-chatbot-container').attr('data-system')).restNonce;
 
-        const $ = cheerio.load(htmlData);
-        const restNonce = JSON.parse($('.mwai-chatbot-container').attr('data-system')).restNonce;
-
-        const submitResponse = await axios.post("https://gpt4free.io/wp-json/mwai-ui/v1/chats/submit", {
-            botId: "default",
-            newMessage: "hy",
-            stream: false,
-        }, {
-            headers: {
-                "X-WP-Nonce": restNonce,
-                "Content-Type": "application/json",
-            },
-        });
-
-        return submitResponse.data;
+        const {
+            data
+        } = await axios(
+            "https://gpt4free.io/wp-json/mwai-ui/v1/chats/submit", {
+                method: "post",
+                data: {
+                    botId: "default",
+                    newMessage: q,
+                    stream: false,
+                },
+                headers: {
+                    "X-WP-Nonce": nonceValue,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        return data.reply;
     } catch (err) {
         console.log(err.response.data);
         return err.response.data.message;
